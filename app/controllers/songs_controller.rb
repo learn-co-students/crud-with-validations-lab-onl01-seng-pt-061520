015ca8a57fafaa64d5b73cfd07df7274b8c6e334
Jade Lynn Masker <jadelynn@masker.family>
2020-11-01 16:48:51 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,35 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    find_song
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+  end
+
+  def edit
+    find_song
+  end
+
+  def update
+    find_song
+  end
+
+  def delete
+    find_song
+  end
+
+  private
+
+  def find_song
+    @song = Song.find_by(params[:id])
+  end
 end
